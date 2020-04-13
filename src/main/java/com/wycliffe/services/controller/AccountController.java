cad960ac1304076f58b25bd01201afcf575722b1
agwanyanjaba <bonkewycliffe@yhoo.com>
2020-04-13 18:52:47 +0300

    "Added fetch Account by ID API"

--- src/main/java/com/wycliffe/services/controller/AccountController.java
+++ src/main/java/com/wycliffe/services/controller/AccountController.java
@@ -1,18 +1,17 @@
 package com.wycliffe.services.controller;
 
 import com.wycliffe.services.model.Account;
+import com.wycliffe.services.model.Authentication;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import com.wycliffe.services.service.AccountService;
 import com.wycliffe.services.utils.RestMetaData;
+
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.HashMap;
@@ -23,42 +22,73 @@ import java.util.List;
 @CrossOrigin(origins = "http://localhost:9000")
 @RequestMapping("/v1/accounts")
 public class AccountController {
-    @Autowired private AccountService accountService;
-    Date date,errorDate = new Date();
+    @Autowired
+    AccountService accountService;
+    Date date, errorDate = new Date();
     private static final Logger LOGGER = LogManager.getLogger(AccountController.class);
 
     //actual API
-    @RequestMapping (value = "",method = RequestMethod.GET,produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
-    public ResponseEntity <HashMap<String,Object>> fetchAccount(){
+    @RequestMapping(value = "", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
+    public ResponseEntity<HashMap<String, Object>> fetchAccount() {
         Long queryStartTime = System.currentTimeMillis();
-        try{
-            List <Account> listAccount = new ArrayList<>();
+        try {
+            List<Account> listAccount = new ArrayList<>();
             listAccount = accountService.getAccounts();
-            RestMetaData restMetaData = new RestMetaData(System.currentTimeMillis()-queryStartTime,date,"Account response");
+            RestMetaData restMetaData = new RestMetaData(System.currentTimeMillis() - queryStartTime, date, "Account response");
 
-            HashMap <String,Object> response  = new HashMap();
-            response.put("MetaData",restMetaData.toString());
-            response.put("Wycliffe Headers","Customers API. Get all customers Data");
-            response.put("Data",listAccount);
+            HashMap<String, Object> response = new HashMap();
+            response.put("MetaData", restMetaData.toString());
+            response.put("Wycliffe Headers", "Customers API. Get all customers Data");
+            response.put("Data", listAccount);
 
             LOGGER.info(listAccount);
             return new ResponseEntity<HashMap<String, Object>>(response, HttpStatus.OK);
 
-        }
-        catch(Exception e){
-            RestMetaData restMetaData = new RestMetaData(System.currentTimeMillis()-queryStartTime,errorDate,"Unexpected Error Occurred");
+        } catch (Exception e) {
+            RestMetaData restMetaData = new RestMetaData(System.currentTimeMillis() - queryStartTime, errorDate, "Unexpected Error Occurred");
             System.out.println(e.getMessage());
-            HashMap <String,Object> response  = new HashMap();
-            response.put("MetaData",restMetaData);
-            response.put("Error",e.getMessage());
-            response.put("Data",null);
+            HashMap<String, Object> response = new HashMap();
+            response.put("MetaData", restMetaData);
+            response.put("Error", e.getMessage());
+            response.put("Data", null);
             return new ResponseEntity<HashMap<String, Object>>(response, HttpStatus.INTERNAL_SERVER_ERROR);
-        }
-        finally {
+        } finally {
             //timing goes here
             System.out.println("Success fetching of account data");
         }
+    }
 
+    //Get account details per user API based on customer ID
+    //API definition
+    @RequestMapping(value = "customer", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
+    public ResponseEntity<HashMap<String, Object>> fetchLogins(@RequestParam String cid) {
+        Long queryStartTime = System.currentTimeMillis();
+        try {
+            //StringUtils.isBlank()
+            List<Account> userAccount = new ArrayList<>();
+            userAccount = accountService.getAccountDetails(cid);
+            Date date = new Date();
 
+            RestMetaData restMetaData = new RestMetaData(System.currentTimeMillis() - queryStartTime, date, "Processing Time");
+            HashMap<String, Object> loginMap = new HashMap<>();
+            loginMap.put("Metadata", restMetaData.toString());
+            loginMap.put("Wycliffe Headers", "Login Data");
+            loginMap.put("Data", userAccount);
+
+            System.out.println(userAccount);
+            return new ResponseEntity<HashMap<String, Object>>(loginMap, HttpStatus.OK);
+        } catch (Exception e) {
+            Date errorDate = new Date();
+            RestMetaData restMetaData = new RestMetaData(System.currentTimeMillis() - queryStartTime, errorDate, "Some error occured");
+            HashMap<String, Object> loginError = new HashMap<>();
+            loginError.put("MetaData", restMetaData.toString());
+            loginError.put("Data", e.getMessage());
+
+            return new ResponseEntity<HashMap<String, Object>>(loginError, HttpStatus.INTERNAL_SERVER_ERROR);
+        } finally {
+            //timing goes here
+            System.out.println("Success fetching of Customer Account Details");
+        }
     }
 }
+
--- src/main/java/com/wycliffe/services/service/AccountService.java
+++ src/main/java/com/wycliffe/services/service/AccountService.java
@@ -1,6 +1,7 @@
 package com.wycliffe.services.service;
 
 import com.wycliffe.services.model.Account;
+import com.wycliffe.services.model.Authentication;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -16,9 +17,9 @@ public class AccountService {
     private JdbcTemplate myJdbcTemplate;
     private static final Logger LOGGER = LogManager.getLogger(AccountService.class);
 
-    public List<Account> getAccounts(){
-        String sqlQuery ="SELECT * FROM customers";
-        return myJdbcTemplate.query(sqlQuery,(rs,rowNum)->{
+    public List<Account> getAccounts() {
+        String sqlQuery = "SELECT * FROM customers";
+        return myJdbcTemplate.query(sqlQuery, (rs, rowNum) -> {
             Account account = new Account();
             account.setCID(rs.getString("cid"));
             account.setPID(rs.getString("pid"));
@@ -33,4 +34,30 @@ public class AccountService {
             return account;
         });
     }
+
+    //Service to get Account based on customer ID
+    //Pass customerID
+    public List<Account> getAccountDetails(String cid) {
+        String accountQuery = "SELECT * FROM customers WHERE cid = ?";
+        return myJdbcTemplate.query(accountQuery, (rs, rowNum) -> {
+            Account userAccount = new Account();
+            userAccount.setCID(rs.getString("cid"));
+            userAccount.setPID(rs.getString("pid"));
+            userAccount.setFirstName(rs.getString("FirstName"));
+            userAccount.setLastName(rs.getString("LastName"));
+            userAccount.setEmail(rs.getString("email"));
+            userAccount.setMSISDN(rs.getString("msisdn"));
+            userAccount.setIMEI(rs.getString("imei"));
+            userAccount.setTOKEN(rs.getString("token"));
+            userAccount.setBalance(rs.getDouble("balance"));
+
+            System.out.println(userAccount);
+            LOGGER.info("Returned Auth info" + userAccount);
+            LOGGER.debug("Returned Auth info" + userAccount);
+            LOGGER.error("Returned Auth info" + userAccount);
+            return userAccount;
+        }, cid
+        );
+    }
+
 }
