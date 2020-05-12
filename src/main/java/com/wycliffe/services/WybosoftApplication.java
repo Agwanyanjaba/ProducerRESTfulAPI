146a0682b976519d7d04cefdef5f36791fe74eb0
agwanyanjaba <bonkewycliffe@yhoo.com>
2020-05-13 00:13:38 +0300

    "Added Final change to All API Controllers"

--- src/main/java/com/wycliffe/services/WybosoftApplication.java
+++ src/main/java/com/wycliffe/services/WybosoftApplication.java
@@ -2,12 +2,13 @@ package com.wycliffe.services;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
-public class WybosoftApplication {
+public class WybosoftApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(WybosoftApplication.class, args);
